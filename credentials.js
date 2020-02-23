4a0bbdf1faf17da84db3e8b9281ac9de38d8a754
Mauri <mauriziobella@gmail.com>
2020-02-23 12:03:16 -0500

    remove credential

--- credentials.js
+++
@@ -1,8 +0,0 @@
-mongo: {
-  development: {
-  connectionString: 'mongodb://localhost:27017/cs546-group6-recipebook-dev'
-  }
-  production: {
-  connectionString: 'mongodb://localhost:27017/cs546-group6-recipebook'
-  }
-}
