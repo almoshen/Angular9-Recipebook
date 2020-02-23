ef23b28903fd37bef7cb981495e8486db2ffda7e
Mauri <mauriziobella@gmail.com>
2020-02-23 11:18:15 -0500

    add express

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// Start the app by listening on the default Heroku port
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
