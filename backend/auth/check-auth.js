f6bd7c33625532232ed767b184eb9a17a65b3ab4
Shen <fsskylark@gmail.com>
2020-03-27 15:59:58 -0400

    changed jwt key to variable

--- backend/auth/check-auth.js
+++ backend/auth/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, 'this_is_used_as_password_for_hashes');
+    jwt.verify(token, process.env.JWT_KEY);
     next();
   } catch (e) {
     res.status(401).json({
