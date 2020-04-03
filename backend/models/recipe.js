5836199f01c8b129fca047c02784323f90f6b6f3
Shen <fsskylark@gmail.com>
2020-04-02 23:29:53 -0400

    added user id to model

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const recipeScheme = mongoose.Schema({
   title: { type: String, required: true },
   instructions: { type: String, required: true},
-  imagePath: { type: String, required: true}
+  imagePath: { type: String, required: true},
+  user: { type: mongoose.Schema.Types.ObjectId, ref:'User', required: true}
 }
 // ,{
 //   versionKey: false
