f160eaee5d71546de7d97601b72ca4fea8586231
Shen <fsskylark@gmail.com>
2020-05-08 23:55:52 -0400

    change it when build

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://recipe-mean.herokuapp.com/'
+  API_URL: 'http://cs546group6recipebook-env.eba-i3c2m2yg.us-east-2.elasticbeanstalk.com'
 };
